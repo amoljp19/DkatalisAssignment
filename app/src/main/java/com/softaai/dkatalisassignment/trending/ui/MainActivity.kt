0d1ab2915b3df19329f1cc3ace34f87d9438f2c7
amol pawar <amoljp19@gmail.com>
2020-02-15 02:29:16 +0530

    final code refactoring

--- app/src/main/java/com/softaai/dkatalisassignment/trending/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/dkatalisassignment/trending/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.softaai.dkatalisassignment.trending.ui
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
