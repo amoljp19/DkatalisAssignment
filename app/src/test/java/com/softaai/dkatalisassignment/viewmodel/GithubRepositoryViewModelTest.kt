395a6ae7d3ec7a95377042ff1a8ff1751d765a45
amol pawar <amoljp19@gmail.com>
2020-03-03 03:09:41 +0530

    GithubRepositoryViewModel Test cases passed

--- app/src/test/java/com/softaai/dkatalisassignment/viewmodel/GithubRepositoryViewModelTest.kt
+++ app/src/test/java/com/softaai/dkatalisassignment/viewmodel/GithubRepositoryViewModelTest.kt
@@ -1,28 +1,68 @@
 package com.softaai.dkatalisassignment.viewmodel
 
+import android.widget.TextView
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.softaai.dkatalisassignment.data.local.GithubRepository
+import com.softaai.dkatalisassignment.databinding.ItemRepositoryBinding
+import com.softaai.dkatalisassignment.di.githubRepositoryViewModelTestModule
 import com.softaai.dkatalisassignment.trending.viewmodel.GithubRepositoryViewModel
+import com.softaai.dkatalisassignment.utils.CircleView
+import io.mockk.impl.annotations.MockK
 import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import org.junit.*
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
-@RunWith(JUnit4::class)
-class GithubRepositoryViewModelTest {
 
-    @Rule
-    @JvmField
-    val instantExecutorRule = InstantTaskExecutorRule()
+class GithubRepositoryViewModelTest: KoinTest{
 
-    private val githubRepositoryViewModel = GithubRepositoryViewModel()
+    private val githubRepositoryViewModel: GithubRepositoryViewModel by inject()
 
+    @MockK
+    lateinit var binding: ItemRepositoryBinding
+
+    @MockK
+    lateinit var circularView: CircleView
+
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this@GithubRepositoryViewModelTest)
+        startKoin{
+            modules(githubRepositoryViewModelTestModule)
+        }
+    }
+
+    @After
+    fun after() {
+        stopKoin()
+    }
 
     @Test
     fun testGithubRepositoryNotNull() {
-        val githubRepository = GithubRepository(
+        githubRepositoryViewModel.repository = getMockGithubRepository()
+        MatcherAssert.assertThat(githubRepositoryViewModel.repository, CoreMatchers.notNullValue())
+    }
+
+    @Test
+    fun testBind(){
+        githubRepositoryViewModel.repository = getMockGithubRepository()
+        Assert.assertEquals("#ffac45", githubRepositoryViewModel.repository?.languageColor)
+        Assert.assertEquals(false, githubRepositoryViewModel.isExpanded)
+    }
+
+    fun getMockGithubRepository():GithubRepository{
+        return GithubRepository(
             id = 0,
             author = "twostraws",
             name = "ControlRoom",
@@ -34,7 +74,5 @@ class GithubRepositoryViewModelTest {
             forks = "70",
             currentPeriodStars = "440"
         )
-        githubRepositoryViewModel.repository = githubRepository
-        MatcherAssert.assertThat(githubRepositoryViewModel.repository, CoreMatchers.notNullValue())
     }
 }
